10c270c489e120e343a61dee06f5dbd28052af17
NahuelNancucheo <nahuel.nancucheo@outlook.com>
2021-06-07 00:24:52 -0300

    First commit

--- checkpoint.js
+++ checkpoint.js
@@ -33,7 +33,17 @@ const {
 // < 16
 
 function exponencial(exp) {
-
+    return function(num) {
+        var value = num ** exp;
+        return value;
+    }
+/*LEAN
+hacemos closure, retornando una nueva funcion
+return function(base) {
+    usamos la var de la funcion padre
+    return Math.pow(base, exp)
+}
+*/
 }
 
 // ----- Recursión -----
@@ -69,9 +79,37 @@ function exponencial(exp) {
 // haciendo los movimientos SUR->ESTE->NORTE
 // Aclaraciones: el segundo parametro que recibe la funcion ('direccion') puede ser pasado vacio (null)
 
-function direcciones(laberinto) {
+function direcciones(laberinto, direccion = "") {
+    //condicion de corte: cuando key === 'destino'
+    //patron de consulta: N, S, E, O.
+    //hacerle consulta al objeto si  tiene una key con value pared
+    //si tiene, pasar a la siguiente key y preguntarle de nuevo
+    //si no tiene, entrar (guardar la key donde se entró)y preguntar si tiene una key con value pared 
+    //y volver a empezar.
 
-}
+    //si no hay laberinto, devuelvo vacio
+    if(!laberinto) return direccion;
+    //por cada llave en laberinto
+    for(let key in laberinto) {
+        //caso base:
+        //si el valor de esa llave es 'destino'
+        if(laberinto[key] === 'destino'){
+            //agrega el nombre de la key a la variable direccion
+            direccion += key;
+            //como llegamos a destino devolve todo el trayecto
+            return direccion;
+        }
+        //si el valor de la llave en la que estamos parado es un objeto, quiere decir que podemos pasar
+        if(typeof laberinto[key] === 'object') {
+            //agrego el nombre de la key a la variable direccion
+            direccion += key;
+            //recursion: vuelvo a validar los pasos anteriores
+            return direcciones(laberinto[key], direccion);
+        }
+    }
+    //en el caso que termine el for in sin destino
+    return direccion;
+};
 
 
 // EJERCICIO 3
@@ -87,8 +125,30 @@ function direcciones(laberinto) {
 // deepEqualArrays([0,1,2], [0,1,2,3]) => false
 // deepEqualArrays([0,1,[[0,1,2],1,2]], [0,1,[[0,1,2],1,2]]) => true
 
-function deepEqualArrays(arr1, arr2) {
-
+function deepEqualArrays(arr1, arr2) { 
+    /*
+    if(typeof arr1[0] !== typeof arr2[0]) return false;
+    if(typeof arr1[0] === typeof arr2[0]) return true; //en vez de 0 iria un i CORREGIR
+    */
+    //si sus largos son diferentes, ya sabemos que es false
+    if(arr1.length !== arr2.length) return false;
+    let flag = false;
+    //si tienen igual length, recorro los indices de arr1
+    for(let i = 0; i < arr1.length; i++) {
+        //me fijo si hay arrays anidados
+        if(Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+            //si la recursion da false, quiere decir que no son iguales(con la negacion entramos al if)
+            if(!deepEqualArrays(arr1[i], arr2[i])) flag = true;
+          //ademas comparo si los numeros son diferentes o no
+        } else if(arr1[i] !== arr2[i]) {
+            //si lo son, retorno false
+            return false;
+        } 
+    };
+    //si la flag es true, significa que algo dio false
+    if(flag) return false;
+    //si pasa todas las condiciones anteriores, es true
+    return true;
 }
 
 
@@ -123,7 +183,7 @@ OrderedLinkedList.prototype.print = function(){
 
 
 // EJERCICIO 4
-// Crea el metodo 'add' que debe agregar nodos a la OLL de forma que la misma se conserve ordenada:
+// Crea el metodo 'add' que debe agregar nodos a la OLL de forma que la misma se conserve ordenada de mayor a menor:
 // Ejemplo:
 // > LL.print()
 // < 'head --> null'
@@ -138,8 +198,44 @@ OrderedLinkedList.prototype.print = function(){
 // > LL.print()
 // < 'head --> 5 --> 3 --> 1 --> null'
 //               4
-OrderedLinkedList.prototype.add = function(val){
-    
+OrderedLinkedList.prototype.add = function(value){ //insert in middle
+   /*
+    if(!this.head) {
+        this.head = new Node(value)
+        return;
+    }
+
+    let current = this.head;
+    while(current.next && current.value > value) {
+        current = current.next;
+    }
+    let aux = current.next;
+    current.next = new Node(value)
+    current.next.next = aux;
+    */
+    let newNode = new Node(val);
+    if (!this.head) {                  // Si la lista estÃ¡ vacÃ­a, agrega this.head             
+        this.head = newNode;
+        return;
+    }
+    if (this.head.value < val) {       // Si head es menor al nuevo, reemplaza 
+        this.head.next = this.head;
+        this.head = newNode;
+        return;
+    }
+    let current = this.head;           // Si head es mayor al nuevo. 
+    while (current.next && current.next.value > val) {  // Mientras exista .next y .next.value sea mayor,
+        current = current.next                          // Recorre la lista hasta no cumplir una de las dos condiciones.    
+    }
+    if (!current.next) {               // Si no existe .next, se llegÃ³ al final de la lista,
+        current.next = newNode;        // y agrega nuevo nodo.
+        return;
+    }
+    let aux = current.next;            // Existe .next y .next.value es menor que el nuevo.
+    current.next = newNode;            // Nuevo reemplaza a .next, que pasa a ser el .next del nuevo.
+    current.next.next = aux;
+    return;
+
 }
 
 
@@ -158,13 +254,19 @@ OrderedLinkedList.prototype.add = function(val){
 // > LL.removeHigher()
 // < null
 
-OrderedLinkedList.prototype.removeHigher = function(){
-    
+OrderedLinkedList.prototype.removeHigher = function(){ //buscar alto y remover
+    //ATENCION A LA LISTA ADD, QUE PIDE AGREGAR ORDENADAMENTE. HACER PRIMERO ESE YA QUE DESPUES REMOVE HIGH Y LOW ES REMOVER HEAD Y COLA.
+    //while(!null) -------->recorrer mientras
+    //recorro como siempre las lista(search)
+    //guardo en auxMayor el primer valor que me encuentro
+    //comparo con el siguiente, es mayor que auxMayor? ---> si lo es., ahora auxmayor es ese siguiente
+    //si no es mayor sigo iterando hasta que next sea null(ahi se termina la lista)
+    //
 }
 
 
 // EJERCICIO 6
-// Crea el metodo 'removeLower' que deve devolver el valor mas bajo de la linked list 
+// Crea el metodo 'removeLower' que debe devolver el valor mas bajo de la linked list 
 // removiendo su nodo corresponidente:
 // Ejemplo:
 // > LL.print()
@@ -178,7 +280,7 @@ OrderedLinkedList.prototype.removeHigher = function(){
 // > LL.removeHigher()
 // < null
 
-OrderedLinkedList.prototype.removeLower = function(){
+OrderedLinkedList.prototype.removeLower = function(){ //buscar bajo y remover
     
 }
 
@@ -210,8 +312,25 @@ OrderedLinkedList.prototype.removeLower = function(){
 //   ];
 // > multiCallbacks(cbs1, cbs2);
 // < ["2-1", "1-1", "1-2", "2-2"];
-
+// T=   1      2      3      4
 function multiCallbacks(cbs1, cbs2){
+    /*
+    var aux = [];
+    for(let value1 of cbs1) {
+       
+    }
+    */
+   //concateno el cbs1 con cbs2, con sort lo ordeno de menor a mayor por la propiedad time que tiene
+   let callbacks = cbs1.concat(cbs2).sort(function(elementA, elementB){
+       return elementA.time - elementB.time;
+   });
+   let results = [];
+   //itero por cada uno de esos callbacks y pusheo el resultado de ejecutar la funcion cb
+   for(let i = 0; i < callbacks.length; i++) {
+       results.push(callbacks[i].cb());
+   };
+   return results;
+    
     
 }
 
@@ -230,8 +349,38 @@ function multiCallbacks(cbs1, cbs2){
 // 5   9
 // resultado:[5,8,9,32,64]
 
-BinarySearchTree.prototype.toArray = function() {
+BinarySearchTree.prototype.toArray = function(current = this, arr = []) {
+    //CASO BASE: si no hay current, llegamos al final del arbol
+    if(!current) {
+        return [];
+    }
+    //tenemos que ir hasta el final del arbol y empezar a devolver valores desde ahi
+    //si hay current, seguimos iterando por la izquierda
+    this.toArray(current.left, arr);
+    //cuando llegamos al final pusheamos 
+    arr.push(current.value);
+    //repetimos con la rama derecha
+    this.toArray(current.right, arr)
+    //retornamos el array
+    return arr;
+
+    /* OTRA MANERA:
+        BimarySearchTree.prototype.toArray = function() {
+            let arr = [];
+            //si existe un left:
+            if(this.left) {
+                arr = arr.concat(this.left.toArray())
+            }
+            //llego al final, no entra al primer if y voy pusheando los values
+            arr.push(this.value);
+            //ahora con la rama derecha
+            if(this.right) {
+                arr = arr.concat(this.right.toArray())
+            }
+            return arr;
+        }
     
+    */
 }
 
 
@@ -267,7 +416,17 @@ function quickSort(array) {
 // para esto hay que unir como right+mid+left o cambiar el 
 // signo menor en la comparacion con el pivot
 
+/* EXTRA CREDITS 2
 
+Esto tal vez les sirva para el extra credit, es una función que cuenta los digitos de un numero (si comienza con cero no lo tiene en cuenta)
+ y no tiene problemas con los negativos. Vi una version parecida sin el Math.floor, pero usa un operador raro (or bit a bit) 
+ 
+ function digitos(num){
+    if(num < 0){ digitos (num *=-1)} 
+    return (num <= 0) ? 0 : Math.floor( Math.log(num) * Math.LOG10E + 1 );
+} 
+
+*/
 
 
 // ----- EXTRA CREDIT -----
@@ -283,6 +442,12 @@ function quickSort(array) {
 // < 32859
 
 function reverse(num){
+    let nuevoNum = 0;
+    while(num > 0) {
+        nuevoNum = nuevoNum * 10 % 10;
+        num = Math.floor(num / 10);
+    }
+    return nuevoNum;
     
 }
 // la grandiosa resolucion de Wilson!!!
